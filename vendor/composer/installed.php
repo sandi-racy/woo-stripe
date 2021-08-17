2b609057a62797fbd169920806165f2e604f3884
sandi_rosyandi <sandiracyyandi@gmail.com>
2021-08-17 16:44:17 +0700

    Installed Stripe

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '222df3739dfa78aa9c7194330b541da2610b2dc0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '222df3739dfa78aa9c7194330b541da2610b2dc0',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.93.0',
+            'version' => '7.93.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => 'c739cf6841bb3498e23bd52ecfadfcb21c521a14',
+            'dev_requirement' => false,
+        ),
+    ),
+);
