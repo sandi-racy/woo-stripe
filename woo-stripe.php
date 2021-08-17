e6a0bef61e60e8796b661f369b5fe2592293411e
sandi_rosyandi <sandiracyyandi@gmail.com>
2021-08-17 14:38:48 +0700

    Registered the class as payment gateway

--- woo-stripe.php
+++ woo-stripe.php
@@ -7,3 +7,18 @@
  * Author URI: https://sandi-racy.github.io/
  * Version: 0.1
  */
+
+// Register the class as payment gateway
+add_filter('woocommerce_payment_gateways', 'woo_stripe_add_gateway_class');
+function woo_stripe_add_gateway_class ($gateways) {
+	$gateways[] = 'WC_Stripe';
+	return $gateways;
+}
+
+// Initialization of payment gateway class
+add_action('plugins_loaded', 'woo_stripe_init_gateway_class');
+function woo_stripe_init_gateway_class () {
+	class WC_Stripe extends WC_Payment_Gateway {
+
+	}
+}
