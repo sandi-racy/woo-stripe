cd701f25a72ab48b4c8d7c747c9d7a2778f8bb06
sandi_rosyandi <sandiracyyandi@gmail.com>
2021-08-17 14:31:56 +0700

    Initial commit

---
+++ woo-stripe.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: Woocommerce Stripe
+ * Plugin URI: https://github.com/sandi-racy/woo-stripe
+ * Description: Stripe payment gateway for Woocommerce
+ * Author: Sandi Rosyandi
+ * Author URI: https://sandi-racy.github.io/
+ * Version: 0.1
+ */
